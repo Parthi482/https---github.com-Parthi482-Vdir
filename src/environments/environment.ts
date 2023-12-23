76a500749be64da991d944ce2246acc8eaf28aea
parthiban <parthiban@kriyatec.com>
2023-12-23 17:20:20 +0530

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,23 @@
+export const environment = {
+  production: false,
+  apiBaseUrl: 'http://10.0.0.123:7000/',
+  // apiBaseUrl: 'http://10.0.0.143:8080/',
+  logoUrl: 'assets/images/logo.png',
+  snackBarDuration: 5000,
+  OrgId: 'pms',
+  cacheBlockSize:14,
+  paginationPageSize:10, 
+  rowModelType:"serverSide",
+  ImageBaseUrl:"https://pms-api.sgp1.digitaloceanspaces.com/"
+};
+export const env = {
+  firebase: {
+  apiKey: "AIzaSyDVDPrN9a52tK8g5BpPCxh0Se7hIoV3T6M",
+  authDomain: "vdirectory-6868b.firebaseapp.com",
+  projectId: "vdirectory-6868b",
+  storageBucket: "vdirectory-6868b.appspot.com",
+  messagingSenderId: "216636554102",
+  appId: "1:216636554102:web:1b76806872777afccb5857",
+  measurementId: "G-0DTPR2B8NH"
+  }
+};
